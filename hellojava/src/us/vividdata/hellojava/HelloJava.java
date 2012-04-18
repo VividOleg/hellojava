f286d8cf150c04c1166d966b33baafb5dd06d571
Oleg Pilipenko <olegp@vividdata.us>
2012-04-18 15:01:54 -0400

    First commit

---
+++ hellojava/src/us/vividdata/hellojava/HelloJava.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package us.vividdata.hellojava;
+
+/**
+ * @author Oleg Pilipenko
+ *
+ */
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
